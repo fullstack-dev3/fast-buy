0755095bfc78cd13c91bcdf7013ffe129c9f1b12
full-stack-david <davidchan12323@gmail.com>
2023-06-06 02:59:28 -0500

    Update Stats tile component

--- src/Tiles.tsx
+++ src/Tiles.tsx
@@ -1,37 +1,31 @@
 const data = [
   {
     "icon": "AiOutlineClockCircle",
-    "color": "text-yellow-600",
     "title": "Pending Orders",
     "count": 20
   },
   {
     "icon": "FaUserAlt",
-    "color": "text-green-600",
     "title": "Total Users",
     "count": 500
   },
   {
     "icon": "GiAbstract010",
-    "color": "text-blue-600",
     "title": "Total Products",
     "count": 1000
   },
   {
     "icon": "CgMenuGridR",
-    "color": "text-purple-600",
     "title": "Total Categories",
     "count": 50
   },
   {
     "icon": "BsClipboardCheck",
-    "color": "text-black",
     "title": "Completed Orders",
     "count": 100
   },
   {
     "icon": "TfiStatsUp",
-    "color": "text-orange-600",
     "title": "Month Statistics",
     "count": 100
   },
--- src/components/StatsTiles.tsx
+++ src/components/StatsTiles.tsx
@@ -9,20 +9,19 @@ import { TfiStatsUp } from 'react-icons/tfi';
 interface Props {
   key: number;
   Icon: any;
-  color: string;
   title: string;
   count: number;
 }
 
-export default function StatsTiles({ Icon, color, title, count }: Props) {
+export default function StatsTiles({ Icon, title, count }: Props) {
   return (
     <div className='m-2 py-6 dark:text-black cursor-pointer hover:shadow-xl transition-all duration-500 bg-white rounded flex flex-col items-center justify-center'>
-      {Icon === 'BsClipboardCheck' && <BsClipboardCheck className={'text-3xl ' + color} />}
-      {Icon === 'GiAbstract010' && <GiAbstract010 className={'text-3xl ' + color} />}
-      {Icon === 'AiOutlineClockCircle' && <AiOutlineClockCircle className={'text-3xl ' + color} />}
-      {Icon === 'FaUserAlt' && <FaUserAlt className={'text-3xl ' + color} />}
-      {Icon === 'CgMenuGridR' && <CgMenuGridR className={'text-3xl ' + color} />}
-      {Icon === 'TfiStatsUp' && <TfiStatsUp className={'text-3xl ' + color} />}
+      {Icon === 'BsClipboardCheck' && <BsClipboardCheck className={'text-3xl text-black'} />}
+      {Icon === 'GiAbstract010' && <GiAbstract010 className={'text-3xl text-blue-600'} />}
+      {Icon === 'AiOutlineClockCircle' && <AiOutlineClockCircle className={'text-3xl text-yellow-600'} />}
+      {Icon === 'FaUserAlt' && <FaUserAlt className={'text-3xl text-green-600'} />}
+      {Icon === 'CgMenuGridR' && <CgMenuGridR className={'text-3xl text-purple-600'} />}
+      {Icon === 'TfiStatsUp' && <TfiStatsUp className={'text-3xl text-orange-600'} />}
       <p className="text-sm mt-1">{title}</p>
       <p className="text-2xl mt-2 font-bold">{count}</p>
     </div>
