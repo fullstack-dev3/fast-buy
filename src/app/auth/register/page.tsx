4449ac207316d8ba7e9119e40ba047ddb982ffe6
full-stack-david <davidchan12323@gmail.com>
2023-07-03 03:32:24 -0500

    Fix build issue in register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -74,7 +74,7 @@ export default function  Register (){
                   </label>
                   <input
                     type="text"
-                    id="namw"
+                    id="name"
                     name="name"
                     placeholder="Name"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5"
