718c1d1dd701731b91cfa8977973a90da2eb3e07
full-stack-david <davidchan12323@gmail.com>
2023-08-04 03:48:03 -0500

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import { Providers } from '@/Store/Provider'
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,7 +7,12 @@ import 'react-toastify/dist/ReactToastify.css';
 const poppin = Poppins({
   weight: ['100', '400'],
   subsets: ['latin'],
-})
+});
+
+export const metadata: Metadata = {
+  title: 'Fast Buy | Online Shop',
+  description: 'Online Shop',
+}
 
 export default function RootLayout({
   children,
